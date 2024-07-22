fa4263dbe79cd674bf0d9d24bbbf5536d1463280
Hugo Heagren <hugo@heagren.com>
2024-07-22 15:54:44 +0100

    Automatically sort by descending date

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((TeX-master . "cv.tex")
+((nil . ((ebib-index-default-sort . ("date" . descend))
+	 (TeX-master . "cv.tex")
 	 (ebib-preload-bib-files . ("~/CV/cv.bib")))))
