0fdc408dfe3d1d7dd671229f40976d56339e9197
Hugo Heagren <hugo@heagren.com>
2025-03-13 20:04:54 +0000

    .dir-lodals.el: include out_dir argument

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "./venv/bin/python3 build.py")
+((nil . ((compile-command . "./venv/bin/python3 build.py /tmp/site")
 	 (compilation-read-command . nil))))
