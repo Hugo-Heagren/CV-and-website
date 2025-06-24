987684de577521a7e10e07e314814ecccb53ea51
Hugo Heagren <hugo@heagren.com>
2025-06-24 10:47:52 +0100

    site: move bib data structure and parsing into separate files

--- site/build.py
+++ site/build.py
@@ -8,11 +8,9 @@ import datetime  # For lastmod times in sitemap URLs
 import unicodedata
 import argparse
 from lxml import etree
-from edtf import parse_edtf, Interval, EDTFObject
 import rcssmin
 import json
-import datatable
-
+from lib import bib_parser as bp
 
 # * Arguments
 
@@ -68,227 +66,6 @@ env = Environment(
 with open(args.info_json_file) as f:
     env.globals["info"] = json.load(f)
 
-
-# ** BibLaTeX data parser
-
-bib_date_components = {"day", "month", "year", "hour", "minute", "second", "timezone"}
-
-
-class BibEntry(dict):
-    """
-    Class representing a single BibLaTeX entry.
-
-    Mostly works just like a dict.
-
-    Dates are stored as EDTFObjects. Getting or setting Keys which
-    correspond with BibLaTeX's date componenets (i.e. day, month,
-    year, etc.) will affect this central date object
-
-    The special key eval_data returns a datatable containing data read
-    in from the file found in the file field (if non-null).
-    """
-
-    def __getitem__(self, key):
-        if key in bib_date_components:
-            date = self.get("date")
-            # This isn't strictly right, but it'll do fine until I
-            # need to handle time properly (i.e. if I ever give two
-            # separate talks on one day...)
-            if isinstance(date, EDTFObject):
-                return getattr(date, key)
-            else:
-                raise KeyError(
-                    f"Cannot get '{key}' because 'date' is not set or is not an EDTFObject"
-                )
-        elif key == "eval_data":
-            val = self.get("eval_data")
-            data_file = self.get("file")
-            if val:
-                return val
-            elif data_file:
-                dt = datatable.fread(data_file)
-                self.eval_data = dt
-                return dt
-            else:
-                raise KeyError(
-                    f"Cannot get '{key}' because entry {self.id} has no data file"
-                )
-        # Fallback to standard dict behaviour
-        else:
-            return super().__getitem__(key)
-
-    def __setitem__(self, key, value):
-        if key in bib_date_components:
-            raise KeyError(
-                f"Cannot set '{key}' because 'date' is not set or is not an EDTFObject"
-            )
-        # Fallback to standard dict behaviour
-        else:
-            super().__setitem__(key, value)
-
-    # This is handy for jinja
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{key}'"
-            )
-
-
-class BibName:
-    def __init__(self, prefix=None, given=None, family=None, suffix=None):
-        self.prefix = prefix
-        self.given = given
-        self.family = family
-        self.suffix = suffix
-
-    def __repr__(self):
-        return (
-            f"BibName(p={self.prefix} g={self.given} f={self.family} s={self.suffix})"
-        )
-
-
-class PageRange:
-    def __init__(self, lower=None, upper=None):
-        self.lower = lower
-        self.upper = upper
-
-
-data_structure_fields = {
-    "list",
-    "names",
-    "name",
-    "namepart",
-    "item",
-    "entry",
-    "entries",
-    "start",
-    "end",
-}
-
-
-class BibLateXMLParser:
-    def __init__(self):
-        self.entries = []
-        self.current_field = None
-        self.current_entry = None
-        # For tracking which part of a name we are recording
-        self.namepart_type = None
-        # Accumulating data
-        # https://stackoverflow.com/a/79547360/14915848
-        self.current_data = ""
-
-    def start(self, tag, attrib):
-        tag_name = etree.QName(tag).localname
-        if tag_name == "entry":
-            self.current_entry = BibEntry()
-            # Default to `misc'
-            self.current_entry["type"] = attrib.get("entrytype", "misc")
-            self.current_entry["id"] = attrib.get("id")
-        elif tag_name == "list":
-            self.current_entry[self.current_field] = []
-        elif tag_name == "names":
-            # List of names
-            name_field = attrib.get("type")
-            self.current_field = name_field
-            self.current_entry[name_field] = []
-        elif tag_name == "name":
-            # Fairly sure names only ever occur as part of namelists.
-            self.current_entry[self.current_field].append(BibName())
-        elif tag_name == "namepart":
-            self.namepart_type = attrib.get("type")
-        elif tag_name == "date" and attrib.get("type", False):
-            # If there's a type, use that
-            type = attrib.get("type")
-            self.current_field = f"{type}date"
-        elif tag_name not in data_structure_fields:
-            self.current_field = tag_name
-
-    def end(self, tag):
-        tag_name = etree.QName(tag).localname
-        string = self.current_data.strip()
-        match tag_name:
-            case "entry":
-                if self.current_entry is not None:
-                    self.entries.append(self.current_entry)
-                    self.current_entry = None
-            case "date":
-                date = None
-                # String date
-                if string != "":
-                    date = parse_edtf(string)
-                    self.current_entry[self.current_field] = date
-                    # Otherwise a range, handled already by
-                    # start/end...
-            # Date ranges
-            case "start":
-                if self.current_field.endswith("date"):
-                    if string == "":
-                        # Special case to cope with open intervals
-                        start_date = None
-                    else:
-                        start_date = parse_edtf(string)
-                    self.current_entry[self.current_field] = Interval(
-                        lower=start_date, upper=None
-                    )
-                elif self.current_field == "pages":
-                    self.current_entry[self.current_field].append(
-                        PageRange(lower=string)
-                    )
-            case "end":
-                if self.current_field.endswith("date"):
-                    if string == "":
-                        # Special case to cope with open intervals
-                        end_date = None
-                    else:
-                        end_date = parse_edtf(string)
-                    self.current_entry[self.current_field].upper = end_date
-                elif self.current_field == "pages":
-                    self.current_entry[self.current_field][-1].upper = string
-            case "item":
-                list_field = self.current_field
-                self.current_entry[list_field].append(string)
-            case "namepart":
-                namelist_field = self.current_field
-                setattr(
-                    self.current_entry[namelist_field][-1], self.namepart_type, string
-                )
-            case _:
-                if (
-                    self.current_entry is not None
-                    and tag_name not in data_structure_fields
-                ):
-                    # Only write if we haven't written already. This
-                    # is because the (entirely whitespace) string
-                    # between the end of one end-tag string and the
-                    # beginning of the following end-tag is recorded
-                    # as 'data, e.g. between these:
-                    #   </bltx:date>
-                    # </bltx:entry>
-                    # In some XML docs, this would be real data, but
-                    # it NEVER is in BibLaTeXML, so (luckily!) we can
-                    # ignore it, with this test.
-                    if not (hasattr(self.current_entry, tag_name)):
-                        self.current_entry[tag_name] = string
-        # Reset state
-        self.current_data = ""
-        if tag_name == "namepart":
-            self.namepart_type = None
-
-    def data(self, data):
-        # Ignore completely empty segments but keep spaces
-        if data.strip():
-            # Append raw data, preserving spaces
-            self.current_data += data
-
-    def close(self):
-        # Reset parser
-        entries = self.entries
-        self.entries = []
-        return entries
-
-
 # ** Eval data schema
 
 # This is useful for getting the shortnames of columns, or filtering
@@ -296,9 +73,9 @@ class BibLateXMLParser:
 with open(repo_dir / "teaching-evals/schema.json") as f:
     env.globals["eval_schema"] = json.load(f)
 
-# ** Get the data
+# ** Bibliography data
 
-biblatex_parser = etree.XMLParser(target=BibLateXMLParser(), remove_blank_text=True)
+biblatex_parser = etree.XMLParser(target=bp.BibLateXMLParser(), remove_blank_text=True)
 
 # Parse the data into the environment
 bib_xml_file = args.bib_xml_file
