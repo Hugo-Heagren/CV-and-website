927b13eee29141d7cb5e484a3dab3205a07315a0
Hugo Heagren <hugo@heagren.com>
2025-07-12 20:00:41 +0100

    localise python command in make

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "make site")
+((nil . ((compile-command . "make site PYTHON=./venv/bin/python3")
 	 (compilation-read-command . nil)
 	 (ebib-index-default-sort . ("date" . descend))
 	 (TeX-master . "cv.tex")
