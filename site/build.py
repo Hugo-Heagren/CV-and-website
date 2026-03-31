27e2887bb0c4eb97de62a7be29940c935b92c8ba
Hugo Heagren <hugo@heagren.com>
2026-03-31 21:25:52 +0100

    build.py: add ordinal method

--- site/build.py
+++ site/build.py
@@ -113,6 +113,17 @@ env.filters["html_mung"] = html_mung
 
 env.filters["rbind"] = rbind
 
+# ** Ordinals
+
+
+def ordinal_string(n):
+    return str(n) + {1: "st", 2: "nd", 3: "rd"}.get(
+        4 if 10 <= n % 100 < 20 else n % 10, "th"
+    )
+
+
+env.filters["ordinal"] = ordinal_string
+
 # * Setup for sitemap
 
 # Variables for sitemap stuff
